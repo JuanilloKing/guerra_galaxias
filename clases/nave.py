bfe4dea8c2db1d9ccb774aa984926e9c8c63e9db
Eduardo <eduardosumarivasalgado@gmail.com>
2026-02-13 16:43:19 +0100

    commit inicial

---
+++ clases/nave.py
@@ -0,0 +1,46 @@
+""" Naves espaciales del juego """
+
+class EstrellaMuerte:
+    """Nave que representa la estrella de la muerte"""
+    def __init__(self, nombre, ataque, defensa, velocidad, coste):
+        self.nombre = nombre
+        self.ataque = ataque
+        self.defensa = defensa
+        self.velocidad = velocidad
+        self.coste = coste
+
+class Ejecutor:
+    """Nave que representa ejecutor"""
+    def __init__(self, nombre, ataque, defensa, velocidad, coste):
+        self.nombre = nombre
+        self.ataque = ataque
+        self.defensa = defensa
+        self.velocidad = velocidad
+        self.coste = coste
+
+class HalconMilenario:
+    """Nave que representa el halcón milenario"""
+    def __init__(self, nombre, ataque, defensa, velocidad, coste):
+        self.nombre = nombre
+        self.ataque = ataque
+        self.defensa = defensa
+        self.velocidad = velocidad
+        self.coste = coste
+
+class NaveRealNaboo:
+    """Nave que representa la nave real de Naboo"""
+    def __init__(self, nombre, ataque, defensa, velocidad, coste):
+        self.nombre = nombre
+        self.ataque = ataque
+        self.defensa = defensa
+        self.velocidad = velocidad
+        self.coste = coste
+
+class CazaEstelarJedi:
+    """Nave caza estelar jedi"""
+    def __init__(self, nombre, ataque, defensa, velocidad, coste):
+        self.nombre = nombre
+        self.ataque = ataque
+        self.defensa = defensa
+        self.velocidad = velocidad
+        self.coste = coste
